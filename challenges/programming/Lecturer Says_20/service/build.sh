d7081a1f4fa4ca7489e3ccbb80e2d2667b9b877c
Yong-Ze <>
2017-09-29 10:56:11 +0800

    Fixed cringeyroll port clash

--- challenges/programming/Lecturer Says_20/service/build.sh
+++ challenges/programming/Lecturer Says_20/service/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build -t lecturersays .
-docker run -d -p 17451:9999 --name lecturersays lecturersays
+docker run -d -p 17122:9999 --name lecturersays lecturersays
 docker start lecturersays
