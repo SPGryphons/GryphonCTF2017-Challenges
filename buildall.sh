5ec68a8056eddac5a1936613b5f26422185368a5
Amos Ng <amosng1@gmail.com>
2017-09-30 23:25:37 +0800

    Added buildall script

---
+++ buildall.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+##
+# Created for GryphonCTF 2017
+# By Amos (LFlare) Ng <amosng1@gmail.com>
+##
+# Set a nice option
+shopt -s globstar
+
+# Remove previous containers
+docker rm -f `docker ps -qa`
+
+# Run all scripts except thee
+for script in challenges/**/[br]*.sh; do
+    parent=`dirname "${script}"`
+    filename=`basename "${script}"`
+    cd "${parent}"
+    ./"${filename}"
+    cd -
+done
+
+# Print out all containers with port bindings
+docker ps --format 'table {{.Names}}\t{{.Ports}}'
