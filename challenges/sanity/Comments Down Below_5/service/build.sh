271b158a4da89aa2c328665638e6fe32b99d0428
CK <exetrtck@gmail.com>
2017-10-02 15:42:06 +0800

    Edited docker files

--- challenges/sanity/Comments Down Below_5/service/build.sh
+++ challenges/sanity/Comments Down Below_5/service/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t san-comments .
+docker build -t comments .
 docker run -it -d -p 17121:80 --restart always --memory 64M --name san-comments comments 
 docker start san-comments
