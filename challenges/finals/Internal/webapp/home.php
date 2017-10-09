7731b6fdec75aea5b3dd2c1c5c28549076e51208
CK <exetrtck@gmail.com>
2017-10-10 02:57:03 +0800

    stlye and rubbish files

--- challenges/finals/Internal/webapp/home.php
+++ challenges/finals/Internal/webapp/home.php
@@ -10,4 +10,4 @@
 <h1>Expect the flag to be here?</h1>
 <h1>Well, this page is not done cos I have a test later :)</h1>
 <h2>Apologies for any inconvinence caused</h2>
-<footer>Copyright @exetr & @void 2017 <br> <a href="ftp.php?file=ftp/tnc.txt">Terms & Conditions</a> apply </footer>
+<footer>Copyright @exetr & @void 2017 <br> <a href="ftp.php?file=tnc.txt">Terms & Conditions</a> apply </footer>
