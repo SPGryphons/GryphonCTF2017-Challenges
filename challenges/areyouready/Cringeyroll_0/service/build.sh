9b3f72ea98d0643f844dbf9af13a04680a9d4925
root <daryl.limkz@gmail.com>
2017-10-10 21:57:37 +0800

    Updated faulty Dockerfiles... As usual

--- challenges/areyouready/Cringeyroll_0/service/build.sh
+++ challenges/areyouready/Cringeyroll_0/service/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t cringeyroll .
-docker run --restart always --memory 64M -d -p 17122:80 --name san-cringeyroll cringeyroll
-docker start san-cringeyroll
+docker run --restart always --memory 64M -d -p 17122:80 --name cringeyroll cringeyroll
+docker start cringeyroll
--- challenges/programming/Find mah monehs_120/service/build.sh
+++ challenges/programming/Find mah monehs_120/service/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t money .
-docker run --restart always --memory 64M -d -p 17454:13337 --name prog-money money
-docker start prog-money
+docker run --restart always --memory 64M -d -p 17454:13337 --name money money
+docker start money
