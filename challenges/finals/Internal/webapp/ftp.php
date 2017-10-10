3f1c8ae5c1230718d4d0a730ab3b7149566b998e
CK <exetrtck@gmail.com>
2017-10-10 10:58:34 +0800

    added more files and updated directory listing

--- challenges/finals/Internal/webapp/ftp.php
+++ challenges/finals/Internal/webapp/ftp.php
@@ -33,7 +33,15 @@
 ?>
 <html>
 <h1>Directory Listing of /ftp</h1>
+<a href="ftp.php?file=Binomial_Theorem_Practice_Questions.pdf" target="_blank">Binomial_Theorem_Practice_Questions.pdf</a>
+<a href="ftp.php?file=Darude-Sandstorm.mp4" target="_blank">Darude-Sandstorm.mp4</a>
+<a href="ftp.php?file=ehd_intro.pptx" target="_blank">ehd_intro.pptx</a>
+<a href="ftp.php?file=ehd_pract01a_protocols.doc" target="_blank">ehd_pract01a_protocols.doc</a>
+<a href="ftp.php?file=password.bck" target="_blank">password.bck</a>
+<a href="ftp.php?file=timetable.PNG" target="_blank">timetable.PNG</a>
 <a href="ftp.php?file=tnc.txt" target="_blank">tnc.txt</a>
+<a href="ftp.php?file=users" target="_blank">users</a>
+<a href="ftp.php?file=welcome.info" target="_blank">welcome.info</a>
 </html>
 <?php
 }
