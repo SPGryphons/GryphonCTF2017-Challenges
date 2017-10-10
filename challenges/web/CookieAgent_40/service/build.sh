ca324f43f542772969233932904044b1ca479d48
root <daryl.limkz@gmail.com>
2017-10-10 21:24:53 +0800

    Edited Service files

--- challenges/web/CookieAgent_40/service/build.sh
+++ challenges/web/CookieAgent_40/service/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+## [ security@dismgryphons.com ]
+## Removed memory limit. Java uses more than 64MB which results in the process getting killed.
 docker build -t cookieagent .
-docker run -dt -p 17561:8080 --name cookieagent cookieagent 
-docker start cookieagent
+docker run --restart always -d -p 17561:8080 --name web-cookieagent cookieagent
+docker start web-cookieagent
