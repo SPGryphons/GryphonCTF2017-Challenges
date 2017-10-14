287cb4ace3eb6bb3542615373c6df6a32f921e5b
zxlim <zxlim1997@gmail.com>
2017-10-14 15:21:31 +0800

    Removed several challenges. #1

--- challenges/reverse/genesis/service/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-## [ security@dismgryphons.com ]
-## Simplified build script
-docker build -t genesis .
-docker run --restart always --memory 64M -dt -p 17234:9999 --name rev-genesis genesis
-docker start rev-genesis
